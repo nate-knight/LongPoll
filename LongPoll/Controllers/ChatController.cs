ee50744794e2980250726e491481a042b409c7fa
nate-knight <nateknight@gmail.com>
2014-03-05 14:05:01 -0600

    first working commit

--- LongPoll/Controllers/ChatController.cs
+++ LongPoll/Controllers/ChatController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LongPoll.Controllers
 {
     public class ChatController : Controller
     {
+        static TaskCompletionSource<string> _nextMessage
+           = new TaskCompletionSource<string>();
+
         //
         // GET: /Chat/
 
@@ -16,6 +21,31 @@ namespace LongPoll.Controllers
             return View();
         }
 
+        public string GetNextMessage()
+        {
+            Thread.Sleep(2000);
+
+            return "GetNextMessage: " + DateTime.Now.ToLongTimeString();
+        }
+
+        public async Task<string> GetNextMessageLongPoll()
+        {
+            //Thread.Sleep(2000);
+
+            //await Task.Delay(2000);
+            //return "GetNextMessageLongPoll " + DateTime.Now.ToLongTimeString() + " count " + counter;
+
+            return await _nextMessage.Task;
+
+        }
+
+
+        public void PostMessage(string message)
+        {
+            _nextMessage.SetResult(message);
+            _nextMessage = new TaskCompletionSource<string>();
+
+        }
 
     }
 }
