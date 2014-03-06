b21098c9f2612fad5b84738bc52d8de72177e014
nate-knight <nateknight@gmail.com>
2014-03-06 15:40:31 -0600

    working simple poll example

---
+++ LongPoll/Controllers/SetTimeoutController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LongPoll.Controllers
+{
+    public class SetTimeoutController : Controller
+    {
+        //
+        // GET: /SetTimeout/
+
+        static List<string> _chats = new List<string>();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public void PostMessage(string message)
+        {
+            _chats.Add(message);
+        }
+
+        public string GetChats()
+        {
+
+            return (_chats.Count > 0) ? string.Join("<br/>", _chats.ToArray()) : ""; 
+
+        }
+
+       
+
+    }
+}
